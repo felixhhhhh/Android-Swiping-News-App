c394b012a26d6531f16ee5f2d4c7dc228beaae57
Felix Hui <57376610+felixhhhhh@users.noreply.github.com>
2021-04-28 10:44:35 -0400

    Update TinNewsDatabase.java

--- app/src/main/java/com/laioffer/newsapp/database/TinNewsDatabase.java
+++ app/src/main/java/com/laioffer/newsapp/database/TinNewsDatabase.java
@@ -1,6 +1,5 @@
 package com.laioffer.newsapp.database;
 
-
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
@@ -9,6 +8,7 @@ import com.laioffer.newsapp.model.Article;
 // version specifies a current version. once introduce or modify, increase the version and define migration strategy
 @Database(entities = {Article.class}, version = 1, exportSchema = false)
 public abstract class TinNewsDatabase extends RoomDatabase {
+    
     // both abstract because room annotation will process
     public abstract ArticleDao articleDao();
 
